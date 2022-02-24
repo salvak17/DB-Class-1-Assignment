43bb1343b7e565b5196951ff0236a8ada232d8ce
salvak17 <67027287+salvak17@users.noreply.github.com>
2022-02-24 13:54:31 -0800

    Update tavernsAssignment.sql
    Removed and added columns to pre-existing tables. Seeded tables with about 5 to 10 rows of data with repeating fields for normalization.
    KDS

--- tavernsAssignment.sql
+++ tavernsAssignment.sql
@@ -61,8 +61,7 @@ DROP TABLE IF EXISTS [Supplies];CREATE TABLE [Supplies](
 	supplyID INT IDENTITY(500,1) PRIMARY KEY,
 	supplyName VARCHAR(100),
 	unit VARCHAR(25),
-	quantity INT,
-	tavID INT FOREIGN KEY REFERENCES Taverns(tavID)
+	cost INT
 );
 
 
@@ -100,3 +99,127 @@ CREATE TABLE [TavernServices](
 	serviceID INT FOREIGN KEY REFERENCES ServicesOffered(serviceID),
 	statusID INT FOREIGN KEY REFERENCES ServiceStatus(statusID)
 );
+
+
+
+-- Seeds table w/info --
+-- Seeds about 5-10 rows w/repeat data --
+INSERT INTO Users (userName, birthday)
+VALUES
+	('Maria Calavera', 1820-10-31),
+	('Yang Xiao Long', 1882-05-25),
+	('Salem', 1873-08-06),
+	('Neon Katt', 1881-12-13),
+	('Sun Wukong', 1881-02-23)
+;
+
+
+
+INSERT INTO Locations (locationName)
+VALUES
+	('Vale'),
+	('Mistral'),
+	('Vacuo'),
+	('Atlas'),
+	('Dark Domain')
+;
+
+
+
+INSERT INTO Taverns (tavName, floorCount, ownerID, locationID)
+VALUES 
+	('Beacon Pub', 65, 101, 300),
+	('Haven Valley', 75, 100, 301),
+	('Signal Pub', 49, 104, 302),
+	('Atlas Inn', 100, 103, 303),
+	('Dark Domain Inn', 80, 102, 304)
+;
+
+
+
+INSERT INTO Roles (roleName)
+VALUES
+	('Chef'),
+	('Bartender'),
+	('Janitor'),
+	('Laundry'),
+	('Accountant')
+;
+
+
+
+INSERT INTO UserRoles (userID, roleID)
+VALUES
+	(102, 204),
+	(104, 201),
+	(101, 201),
+	(103, 200),
+	(100, 204)
+;
+
+
+
+INSERT INTO BasementRats(ratName, tavID)
+VALUES
+	('Jaq', 1),
+	('Gus', 1),
+	('Suzy', 4),
+	('Perla', 2),
+	('Bert', 3)
+;
+
+
+
+
+INSERT INTO Supplies(supplyName, unit, cost)
+VALUES
+	('toilet paper', 'by case of 6', 3),
+	('chicken whole', 'ounces', 5),
+	('russet potatoes whole', 'pounds', 1),
+	('carrots whole', 'pounds', 1),
+	('borboun', 'ounces', 2)
+;
+
+
+
+INSERT INTO TavernInventory(tavID, supplyID, cost, quantity, dateRecieved)
+VALUES
+	(1, 501, 225, 75, 1900-10-21),
+	(1, 502, 50, 50, 1900-10-25),
+	(1, 503, 50, 50, 1900-10-25),
+	(3, 500, 10, 30, 1900-10-06),
+	(4, 504, 400, 200, 1900-10-21)
+;
+
+
+
+INSERT INTO ServicesOffered(serviceName)
+VALUES
+	('laundry'),
+	('mopping'),
+	('sweeping'),
+	('cooking'),
+	('bartending'),
+	('accounting')
+;
+
+
+
+INSERT INTO ServiceStatus(statusName)
+VALUES
+	('active'),
+	('inactive')
+;
+
+
+
+INSERT INTO TavernServices(tavID, serviceID, statusID)
+VALUES
+	(1, 603, 701),
+	(1, 604, 700),
+	(5, 604, 700),
+	(4, 603, 700),
+	(4, 604, 701),
+	(3, 601, 701),
+	(3, 602, 701)
+;
