bf5058b84aa31aaabf4f06d4853ab9c3a68d9d5f
salvak17 <67027287+salvak17@users.noreply.github.com>
2022-02-24 15:06:32 -0800

    Update tavernsAssignment.sql
    Got rid of unnecessary brackets.
    KDS

--- tavernsAssignment.sql
+++ tavernsAssignment.sql
@@ -4,8 +4,8 @@ USE class1TavernAssignment;
 
 
 -- Creates all tables for tavern assignment --
-DROP TABLE IF EXISTS [Users];
-CREATE TABLE[Users](
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users(
 	userID INT IDENTITY(100,1) PRIMARY KEY,
 	userName VARCHAR(150),
 	birthday DATE
@@ -13,16 +13,16 @@ CREATE TABLE[Users](
 
 
 
-DROP TABLE IF EXISTS [Locations];
-CREATE TABLE [Locations](
+DROP TABLE IF EXISTS Locations;
+CREATE TABLE Locations(
 	locationID INT IDENTITY(300,1) PRIMARY KEY,
 	locationName VARCHAR(150)
 );
 
 
 
-DROP TABLE IF EXISTS [Taverns];
-CREATE TABLE[Taverns](
+DROP TABLE IF EXISTS Taverns;
+CREATE TABLE Taverns(
 	tavID INT IDENTITY(1,1) PRIMARY KEY,
 	tavName VARCHAR(150),
 	floorCount INT,
@@ -32,24 +32,24 @@ CREATE TABLE[Taverns](
 
 
 
-DROP TABLE IF EXISTS [Roles];
-CREATE TABLE[Roles](
+DROP TABLE IF EXISTS Roles;
+CREATE TABLE Roles(
 	roleID INT IDENTITY(200,1) PRIMARY KEY,
 	roleName VARCHAR(50)
 );
 
 
 
-DROP TABLE IF EXISTS [UserRoles];
-CREATE TABLE[UserRoles](
+DROP TABLE IF EXISTS UserRoles;
+CREATE TABLE UserRoles(
 	userID INT FOREIGN KEY REFERENCES Users(userID),
 	roleID INT FOREIGN KEY REFERENCES Roles(roleID)
 );
 
 
 
-DROP TABLE IF EXISTS [BasementRats];
-CREATE TABLE [BasementRats](
+DROP TABLE IF EXISTS BasementRats;
+CREATE TABLE BasementRats(
 	ratID INT IDENTITY(400,1) PRIMARY KEY,
 	ratName VARCHAR(75),
 	tavID INT FOREIGN KEY REFERENCES Taverns(tavID)
@@ -57,7 +57,8 @@ CREATE TABLE [BasementRats](
 
 
 
-DROP TABLE IF EXISTS [Supplies];CREATE TABLE [Supplies](
+DROP TABLE IF EXISTS Supplies;
+CREATE TABLE Supplies(
 	supplyID INT IDENTITY(500,1) PRIMARY KEY,
 	supplyName VARCHAR(100),
 	unit VARCHAR(25),
@@ -66,8 +67,8 @@ DROP TABLE IF EXISTS [Supplies];CREATE TABLE [Supplies](
 
 
 
-DROP TABLE IF EXISTS [TavernInventory];
-CREATE TABLE [TavernInventory](
+DROP TABLE IF EXISTS TavernInventory;
+CREATE TABLE TavernInventory(
 	tavID INT FOREIGN KEY REFERENCES Taverns(tavID),
 	supplyID INT FOREIGN KEY REFERENCES Supplies(supplyID),
 	cost INT,
@@ -77,24 +78,24 @@ CREATE TABLE [TavernInventory](
 
 
 
-DROP TABLE IF EXISTS [ServicesOffered];
-CREATE TABLE [ServicesOffered](
+DROP TABLE IF EXISTS ServicesOffered;
+CREATE TABLE ServicesOffered(
 	serviceID INT IDENTITY(600,1) PRIMARY KEY,
 	serviceName VARCHAR(75)
 );
 
 
 
-DROP TABLE IF EXISTS [ServiceStatus];
-CREATE TABLE [ServiceStatus](
+DROP TABLE IF EXISTS ServiceStatus;
+CREATE TABLE ServiceStatus(
 	statusID INT IDENTITY(700,1) PRIMARY KEY,
 	statusName VARCHAR(25)
 );
 
 
 
-DROP TABLE IF EXISTS [TavernServices];
-CREATE TABLE [TavernServices](
+DROP TABLE IF EXISTS TavernServices;
+CREATE TABLE TavernServices(
 	tavID INT FOREIGN KEY REFERENCES Taverns(tavID),
 	serviceID INT FOREIGN KEY REFERENCES ServicesOffered(serviceID),
 	statusID INT FOREIGN KEY REFERENCES ServiceStatus(statusID)
